02c555c25a4b7957337717b78bdee354e85c457f
HieuMinh19 <lehieuminh231@gmail.com>
2022-07-23 23:02:41 +0700

    building default template

---
+++ components/Footer.vue
@@ -0,0 +1,284 @@
+<template>
+  <footer class="footer" role="contentinfo">
+    <div class="flex-footer standard-padding">
+      <div class="menu-footer-container">
+        <ul id="menu-footer" class="menu">
+          <li
+            id="menu-item-7321"
+            class="
+              menu-item
+              col-md-4
+              menu-item-type-custom
+              menu-item-object-custom
+              menu-item-has-children
+            "
+          >
+            <a href="#">Travel to Israel</a>
+            <ul class="sub-menu">
+              <li
+                id="menu-item-24056"
+                class="
+                  menu-item
+                  col-md-4
+                  menu-item-type-custom menu-item-object-custom menu-item-24056
+                "
+              >
+                <a href="https://www.touristisrael.com/tours/"
+                  >Tourist Israel Tours</a
+                >
+              </li>
+              <li
+                id="menu-item-24055"
+                class="
+                  menu-item
+                  menu-item-type-post_type
+                  menu-item-object-page menu-item-24055
+                "
+              >
+                <a href="https://www.touristisrael.com/hotels/"
+                  >Tourist Israel Hotels</a
+                >
+              </li>
+              <li
+                id="menu-item-38844"
+                class="
+                  menu-item
+                  menu-item-type-taxonomy
+                  menu-item-object-type
+                  menu-item-38844
+                "
+              >
+                <a href="https://www.touristisrael.com/type/package-tours/"
+                  >Israel Package Tours</a
+                >
+              </li>
+              <li
+                id="menu-item-38847"
+                class="
+                  menu-item
+                  menu-item-type-custom
+                  menu-item-object-custom
+                  menu-item-38847
+                "
+              >
+                <a href="https://www.touristisrael.com/type/private-tours/"
+                  >Groups &amp; Custom Tours</a
+                >
+              </li>
+              <li
+                id="menu-item-38843"
+                class="
+                  menu-item
+                  menu-item-type-taxonomy
+                  menu-item-object-type
+                  menu-item-38843
+                "
+              >
+                <a href="https://www.touristisrael.com/type/private-tours/"
+                  >Private Tours in Israel</a
+                >
+              </li>
+              <li
+                id="menu-item-51025"
+                class="
+                  menu-item
+                  menu-item-type-custom
+                  menu-item-object-custom
+                  menu-item-51025
+                "
+              >
+                <a
+                  href="https://www.touristisrael.com/destination/dead-sea-and-masada-tours/"
+                  >Dead Sea &amp; Masada Tours</a
+                >
+              </li>
+              <li
+                id="menu-item-51027"
+                class="
+                  menu-item
+                  menu-item-type-custom
+                  menu-item-object-custom
+                  menu-item-51027
+                "
+              >
+                <a
+                  href="https://www.touristisrael.com/destination/jerusalem-tours/"
+                  >Jerusalem Tours</a
+                >
+              </li>
+              <li
+                id="menu-item-51029"
+                class="
+                  menu-item
+                  menu-item-type-custom
+                  menu-item-object-custom
+                  menu-item-51029
+                "
+              >
+                <a
+                  href="https://www.touristisrael.com/destination/tel-aviv-tours/"
+                  >Tel Aviv Tours</a
+                >
+              </li>
+            </ul>
+          </li>
+          <li
+            id="menu-item-7328"
+            class="
+              menu-item
+              menu-item-type-custom
+              menu-item-object-custom
+              menu-item-has-children
+              menu-item-7328
+            "
+          >
+            <a href="#">Need Help?</a>
+            <ul class="sub-menu">
+              <li
+                id="menu-item-7331"
+                class="
+                  menu-item
+                  menu-item-type-post_type
+                  menu-item-object-page menu-item-7331
+                "
+              >
+                <a href="https://www.touristisrael.com/about/">About Us</a>
+              </li>
+              <li
+                id="menu-item-48901"
+                class="
+                  menu-item
+                  menu-item-type-post_type
+                  menu-item-object-page menu-item-48901
+                "
+              >
+                <a
+                  href="https://www.touristisrael.com/contact/?ref=eyJyZWZfaWQiOiIxNTMyNCIsInJlZl90aXRsZSI6IlBldHJhLCBXYWRpIFJ1bSAmIzAzODsgSGlnaGxpZ2h0cyBvZiBKb3JkYW4gJiM4MjExOyAzIERheSBUb3VyIGZyb20gSmVydXNhbGVtIG9yIFRlbCBBdml2IiwicmVmX3VybCI6Imh0dHBzOi8vd3d3LnRvdXJpc3Rpc3JhZWwuY29tL3RvdXJzL3BldHJhLXdhZGktcnVtLTMtZGF5LXRvdXItamVydXNhbGVtLyIsInJlZl9jb250ZXh0IjoiRm9vdGVyIE1lbnUiLCJyZWZfcHJpY2UiOiI1ODkiLCJyZWZfcHJpY2VfdHlwZSI6InBlcnNvbiIsInJlZl9wcmljZV9zdHJpbmciOiIkNTg5IHBlciBQZXJzb24ifQ==&amp;it=0"
+                  >Contact Us</a
+                >
+              </li>
+              <li
+                id="menu-item-38845"
+                class="
+                  menu-item
+                  menu-item-type-post_type
+                  menu-item-object-page menu-item-38845
+                "
+              >
+                <a href="https://www.touristisrael.com/press-coverage/"
+                  >Press Coverage</a
+                >
+              </li>
+              <li
+                id="menu-item-38851"
+                class="
+                  menu-item
+                  menu-item-type-post_type
+                  menu-item-object-page menu-item-38851
+                "
+              >
+                <a href="https://www.touristisrael.com/travel-with-passion/"
+                  >Travel with Passion</a
+                >
+              </li>
+              <li
+                id="menu-item-50487"
+                class="
+                  menu-item
+                  menu-item-type-post_type
+                  menu-item-object-page menu-item-50487
+                "
+              >
+                <a href="https://www.touristisrael.com/careers/">Careers</a>
+              </li>
+              <li
+                id="menu-item-38846"
+                class="
+                  menu-item
+                  menu-item-type-post_type
+                  menu-item-object-page menu-item-38846
+                "
+              >
+                <a href="https://www.touristisrael.com/travel-agents/"
+                  >Israel Travel Agent</a
+                >
+              </li>
+              <li
+                id="menu-item-39279"
+                class="
+                  menu-item
+                  menu-item-type-post_type
+                  menu-item-object-page menu-item-39279
+                "
+              >
+                <a href="https://www.touristisrael.com/terms-conditions/"
+                  >Terms &amp; Conditions</a
+                >
+              </li>
+              <li
+                id="menu-item-39280"
+                class="
+                  menu-item
+                  menu-item-type-post_type
+                  menu-item-object-page menu-item-privacy-policy menu-item-39280
+                "
+              >
+                <a href="https://www.touristisrael.com/privacy-policy/"
+                  >Privacy &amp; Cookie Policy</a
+                >
+              </li>
+              <li
+                id="menu-item-39284"
+                class="
+                  menu-item
+                  menu-item-type-post_type
+                  menu-item-object-page menu-item-39284
+                "
+              >
+                <a
+                  href="https://www.touristisrael.com/tourist-israel-cancellation-policy-refund-tour/"
+                  >Cancellation Policy</a
+                >
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="copyright standard-padding">
+      © 2022
+      <a target="_blank" href="https://www.touristisrael.com/"
+        >Tourist Israel</a
+      >
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  font-size: 18px;
+  line-height: 22px;
+  display: block;
+  width: 100%;
+  color: white;
+
+  .flex-footer {
+    padding-top: 50px;
+    padding-bottom: 20px;
+    background-color: #312f2c;
+  }
+
+  .menu {
+    display: flex;
+  }
+
+  .flex-footer .menu-footer-container ul {
+    // box-sizing: border-box;
+    // width: calc(100% + 20px);
+    // margin-left: -10px;
+    // list-style: none;
+    // justify-content: space-between;
+    // flex-wrap: wrap;
+  }
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,130 @@
+<template>
+  <div id="header">
+    <div class="top-bar">
+      <ul>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li><a href="#">contact</a></li>
+        <li><a href="#">login</a></li>
+        <li><a href="#">Sign Up</a></li>
+      </ul>
+    </div>
+    <header class="header" role="banner">
+      <div class="left-pane">
+        <a
+          class="ti-logo"
+          href="https://www.touristisrael.com"
+          style="background-color: rgb(249, 134, 77)"
+          ><img
+            src="https://www.touristisrael.com/wp-content/themes/tourist-israel/images/logo-ti.svg"
+            alt="Tourist Israel Logo"
+            loading="eager"
+          />
+          <p>Tours</p></a
+        >
+      </div>
+
+      <nav>
+        <div class="nav-menu">
+          <ul>
+            <li>TOURS & THINKS TO DO</li>
+            <li>PACKAGES</li>
+            <li>HOTELS & STAYS</li>
+            <li>PLACES TO VISIT</li>
+            <li>SEARCH</li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#header {
+  font-family: "bebas-neue-pro-expanded";
+
+  .top-bar {
+    display: flex;
+    justify-content: flex-end;
+    padding: 0 7.4%;
+    background-color: #fdfcec;
+
+    li {
+      display: inline;
+      list-style: none;
+      border-right: 1px solid #938a82;
+      margin-top: 1px;
+      vertical-align: middle;
+      padding: 0 12px 0 8px;
+
+      a {
+        font-size: 14px;
+        line-height: 16px;
+        color: #151209;
+        text-transform: capitalize;
+        text-decoration: none;
+      }
+    }
+  }
+
+  header {
+    position: relative;
+    z-index: 140000;
+    display: flex;
+    padding: 0 7.4%;
+    background-color: #feef43;
+    justify-content: space-between;
+
+    .ti-logo {
+      position: absolute;
+      z-index: 1000000;
+      top: -40px;
+      display: block;
+      width: 200px;
+      transition: background-color 0.1s ease-in-out;
+      background-color: #f5854c;
+
+      p {
+        font-size: 24px;
+        font-weight: 600;
+        line-height: 36px;
+        position: absolute;
+        bottom: 0;
+        padding: 10px 18px;
+        text-transform: uppercase;
+        color: #feef43;
+        margin: unset;
+      }
+    }
+    .lef-banner {
+      display: flex;
+      min-height: 90px;
+    }
+
+    nav {
+      .nav-menu {
+        ul li {
+          min-height: 90px;
+          font-size: 24px;
+          font-weight: 600;
+          line-height: 40px;
+          color: inherit;
+          display: inline-flex;
+          margin: 0 0 0 30px;
+          list-style: none;
+          transition: color 0.15s ease-in-out;
+          text-transform: uppercase;
+          align-items: center;
+        }
+      }
+    }
+  }
+
+  @media only screen and (max-width: 1400px) {
+    nav {
+      padding: 0 40px;
+    }
+  }
+}
+</style>
